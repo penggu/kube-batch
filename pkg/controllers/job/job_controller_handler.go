e0a2a8edb6dae83559e407f5dd430818441537df
limingzhe 00427178 <limingzhe@huawei.com>
2019-04-30 17:36:14 +0800

    job phase is wrong when pod success fast

--- pkg/controllers/job/job_controller_handler.go
+++ pkg/controllers/job/job_controller_handler.go
@@ -81,13 +81,13 @@ func (cc *Controller) updateJob(oldObj, newObj interface{}) {
 
 	//NOTE: Since we only reconcile job based on Spec, we will ignore other attributes
 	// For Job status, it's used internally and always been updated via our controller.
-	if reflect.DeepEqual(newJob.Spec, oldJob.Spec) {
+	if reflect.DeepEqual(newJob.Spec, oldJob.Spec) && newJob.Status.State.Phase == oldJob.Status.State.Phase {
 		glog.Infof("Job update event is ignored since no update in 'Spec'.")
 		return
 	}
 
 	if err := cc.cache.Update(newJob); err != nil {
-		glog.Errorf("Failed to update job <%s/%s>: %v in cache",
+		glog.Errorf("UpdateJob - Failed to update job <%s/%s> in cache: %v ",
 			newJob.Namespace, newJob.Name, err)
 	}
 
